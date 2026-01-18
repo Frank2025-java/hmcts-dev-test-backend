0903f531de4ecf1236dde9daff78ddcb4972a584
frankz2025-gitlab <frankz@iae.nl>
2026-01-18 19:57:42 +0000

    Readme, and moved Application to spring package

--- src/main/java/uk/co/frankz/hmcts/dts/spring/Application.java
+++ src/main/java/uk/co/frankz/hmcts/dts/spring/Application.java
@@ -1,4 +1,4 @@
-package uk.co.frankz.hmcts.dts;
+package uk.co.frankz.hmcts.dts.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

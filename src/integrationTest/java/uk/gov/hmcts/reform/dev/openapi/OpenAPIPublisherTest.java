3041113e1f1457e55a12592ff9aece7cf768b2f1
frankz2025-gitlab <frankz@iae.nl>
2026-04-10 22:47:46 +0100

    upload openApi specs like a gitlab artifact

--- src/integrationTest/java/uk/gov/hmcts/reform/dev/openapi/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/dev/openapi/OpenAPIPublisherTest.java
@@ -10,22 +10,41 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import uk.co.frankz.hmcts.dts.spring.Application;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * This test will be run by ${workspace}\.github\workflows\publish-openapi.yaml
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = Application.class)
 class OpenAPIPublisherTest {
 
-    @Value("${TEST_URL:http://localhost:4000}")
-    private String testUrl;
+    private final String testUrl;
+
+    private final String outputFilePath;
+
+    private final ResourceLoader resourceLoader;
+
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    private ResourceLoader resourceLoader;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+    public OpenAPIPublisherTest(
+        @Value("${TEST_URL:http://localhost:4000}") String testUrl,
+        @Value("${API_DOC_PATH:build/openapi/openapi-spec.json}") String outputFilePath,
+        ResourceLoader resourceLoader,
+        ObjectMapper objectMapper
+    ) {
+        this.testUrl = testUrl;
+        this.outputFilePath = outputFilePath;
+        this.resourceLoader = resourceLoader;
+        this.objectMapper = objectMapper;
+    }
 
     @Test
-    void shouldGenerateValidOpenApiSpec() throws Exception {
+    void shouldGenerateValidOpenApiSpecFollowedByUpload() throws Exception {
         String openApiUrl = testUrl + "/v3/api-docs";
 
         Resource resource = resourceLoader.getResource(openApiUrl);
@@ -41,6 +60,14 @@ class OpenAPIPublisherTest {
 
         // See it again: gradle integration --rerun --tests uk.gov.hmcts.reform.dev.openapi.OpenAPIPublisherTest --info
         String pretty = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+
+        // Print to console (still useful locally)
         System.out.println(pretty);
+
+        // Write to a file for GitHub Actions
+        Path output = Paths.get(outputFilePath);
+        Files.createDirectories(output.getParent());
+        Files.writeString(output, pretty);
+        System.out.println("Uploaded as " + output);
     }
 }
